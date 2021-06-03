ec5147e35215d2596609dcd4e5fa2c57301cdb1f
Max Block <maxblock@pm.me>
2021-06-03 19:21:01 +0300

    update

--- mb_commons/__init__.py
+++ mb_commons/__init__.py
@@ -5,4 +5,4 @@ from .http import CHROME_USER_AGENT, FIREFOX_USER_AGENT, HResponse, hrequest  # 
 from .result import Result  # noqa: F401
 from .str import number_with_separator, str_to_list  # noqa: F401
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
